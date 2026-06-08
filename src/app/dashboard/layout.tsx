03539f7c385bad9c6cef3572f4ad12fe8b6b5216
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-06-08 22:55:41 +0530

    fix mobile button size and spacing

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,7 @@ export default async function DashboardLayout({
   return (
     <div className="min-h-screen bg-slate-50 flex">
       <Sidebar ownerName={owner.name} />
-      <main className="flex-1 lg:ml-64 p-6 lg:p-8 pt-16 lg:pt-8">
+      <main className="flex-1 lg:ml-64 px-4 pt-16 pb-6 lg:p-8 lg:pt-8">
         <div className="max-w-7xl mx-auto">{children}</div>
       </main>
     </div>
